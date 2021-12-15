3e49a58f71a5d323e01c4662aba754c7c9b280be
Zverev78С <9842905@gmail.com>
2021-12-15 23:55:04 +0300

    Создал ветвь и проект для чет/нечет

---
+++ HW_even-odd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_even_odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Введите целое число:");
+
+            Console.ReadKey();  // консоль ожидает нажатия любой кнопки для закрытия 
+        }
+    }
+}
