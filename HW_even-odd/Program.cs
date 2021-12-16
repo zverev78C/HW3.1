abdd30c1bdb67405c9f531530644b5d188e69bfd
zverev78 <9842905@gmail.com>
2021-12-16 12:02:30 +0300

    решение чёт не чет

--- HW_even-odd/Program.cs
+++ HW_even-odd/Program.cs
@@ -11,8 +11,17 @@ namespace HW_even_odd
         static void Main(string[] args)
 
         {
-            Console.WriteLine("Введите целое число:");
+            Console.WriteLine("Введите целое число:");    // программа просит пользователя ввести число
+            int number = int.Parse( Console.ReadLine());  // переменная для введенного числа
 
+            if (number % 2 == 0)  // определение четности числа через деление с остатком
+            {
+                Console.WriteLine("Число четное");  // действие при четности числа
+            }
+            else
+            {
+                Console.WriteLine("Число не четное");  // действие при не четности числа
+            }
             Console.ReadKey();  // консоль ожидает нажатия любой кнопки для закрытия 
         }
     }
