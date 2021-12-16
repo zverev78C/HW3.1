b33613a1a2a2ce16ed0b9b2fe43a5f5db16b322a
zverev78 <9842905@gmail.com>
2021-12-16 16:52:00 +0300

    создал решение простые числа

---
+++ Prime Namders/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Namders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Insert number");
+            int N = int.Parse(Console.ReadLine());
+            int D;
+            while (N %)
+            {
+
+                N++;
+            }
+
+
+
+        }
+    }
+}
